72b521b36936339153c1594390a4e65f1d49bbc2
Said Said <ssaid20@icloud.com>
2023-10-05 16:34:12 -0500

    implemented search git

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import FavoriteList from '../FavoriteList/FavoriteList.jsx';
+import Search from '../Search/Search.jsx';
 
 function App(props) {
   return (
     <div>
       <h1>Giphy Search!</h1>
       <h2>Hi!</h2>
-      <FavoriteList/ >
+      <Search />
+      <FavoriteList />
+      
     </div>
+    
   );
 }
 
 export default App;
+
